5756bbd7915f14b58dce55c3d5b7b29a125549c3
Samuel Peter <you@example.com>
2022-04-09 22:55:17 +0000

    first

---
+++ test.py
@@ -0,0 +1,16 @@
+import uvicorn
+import VoiceAssistant
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.post("/calls")
+async def welome(resp):
+    response = '''<Response>
+            <Play url="/KaraBankVoice/Welcome.mp3"/>
+            </Response>'''
+
+if __name__ == '__main__':
+    app.run(debug=True)
