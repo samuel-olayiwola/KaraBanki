1d628e48d69ae85dba56da7e3e897a047bc63eb1
Samuel Peter <you@example.com>
2022-04-12 05:36:37 +0000

    edit

--- app/main.py
+++ app/main.py
@@ -14,44 +14,44 @@ import os
 app= Flask(__name__)
 @app.post('/')
 def voice():
-  session_id   = request.values.get("sessionId", None)
-  isActive  = request.values.get("isActive", None)
-  response = ""
-  response =  '''<Response>
-         <Say>Welcome to karabanki my dear</Say>
-         </Response>'''
-  return response
-
-
-#   if(isActive == 1):
-#      phone_number = request.values.get("callerNumber", None)
+   session_id   = request.values.get("sessionId", None)
+   isActive  = request.values.get("isActive", None)
+   response = ""
+   response =  '''<Response>
+            <Say>Welcome to karabanki my dear</Say>
+            '''
+   if isActive == 1:
+      phone_number = request.values.get("callerNumber", None)
    
-    
-  # if(phone_number != None):
-  #   if(VoiceAssistant.isCustomer(phone_number.replace("+234",""))):
-  #     commands = VoiceAssistant.voiceComands()
-  #     for command in commands:
-  #       response += command
-  #   else:
-  #     VoiceAssistant.voiceOpenAcct()
-  #   response += ' <GetDigits timeout="30" finishOnKey="#">'
-  #   response += '<Say voice="man" playBeep="false">Please enter your account '
-  #   response += 'number followed by the hash sign</Say> </GetDigits> </Response>'
+   if(phone_number != None):
+      if(VoiceAssistant.isCustomer(phone_number.replace("+234",""))):
+         commands = VoiceAssistant.voiceComands()
+         for command in commands:
+            response += command
+         response += "</Response>"
+      else:
+         commands = VoiceAssistant.voiceOpenAcct()
+         response += "</Response>"
+   # else:
+   #    commands = VoiceAssistant.voiceOpenAcct()
+   #    response += ' <GetDigits timeout="30" finishOnKey="#">'
+   #    response += '<Say voice="man" playBeep="false">Please enter your account '
+   #    response += 'number followed by the hash sign</Say> </GetDigits> </Response>'
 
-  #   dtmfDigits = request.values.get("dtmfDigits", None)
+   #  dtmfDigits = request.values.get("dtmfDigits", None)
 
-  #   if dtmfDigits == '1234':
-  #     response = '<Response> <GetDigits timeout="30" finishOnKey="#">'
-  #     response +=' <Say voice="man" playBeep="false"> Press 1 followed by a hash '
-  #     response +='sign to get your account balance or 0 followed by a hash sign to'
-  #     response += ' quit</Say> </GetDigits></Response>'
+   #  if dtmfDigits == '1234':
+   #    response = '<Response> <GetDigits timeout="30" finishOnKey="#">'
+   #    response +=' <Say voice="man" playBeep="false"> Press 1 followed by a hash '
+   #    response +='sign to get your account balance or 0 followed by a hash sign to'
+   #    response += ' quit</Say> </GetDigits></Response>'
 
-  #   elif dtmfDigits == '1':
-  #     response = '<Response>'
-  #     response += '<Say voice="man" playBeep="false" >Your balance is 1234 Shillings</Say>'
-  #     response+= '<Reject/> </Response>'
+   #  elif dtmfDigits == '1':
+   #    response = '<Response>'
+   #    response += '<Say voice="man" playBeep="false" >Your balance is 1234 Shillings</Say>'
+   #    response+= '<Reject/> </Response>'
 
-  #   elif dtmfDigits == '0':
-  #     response = '<Response>'
-  #     response += '<Say voice="man" playBeep="false" >Its been a pleasure, good bye </Say>'
-  #     response+= '<Reject/> </Response>'
+   #  elif dtmfDigits == '0':
+   #    response = '<Response>'
+   #    response += '<Say voice="man" playBeep="false" >Its been a pleasure, good bye </Say>'
+   #    response+= '<Reject/> </Response>'
