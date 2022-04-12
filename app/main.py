fa09bc8dbcaf3f4b1b740000adc2f9bd702706bf
Samuel Peter <you@example.com>
2022-04-12 09:11:01 +0000

    edit

--- app/main.py
+++ app/main.py
@@ -25,27 +25,33 @@ def voiceComands():
 '''play buy airtime command'''
 def voiceAirtime():
    print("playing voiceairtime")
+   return '<Say> For airtime, Press 1</say> '
    return '<Play url="https://drive.google.com/file/d/1s-4UooIKrH1LhDp4MVdhNyFaJ6og9ezc/view?usp=sharing"/>'
 
 
 '''play open account command'''
 def voiceOpenAcct():
     print("playing openacct message")
+    return '<Say> To register, Press 1 </Say>'
     return '<Play url="https://drive.google.com/file/d/1Ur37PMtVAE_ZD-eGbcx8hC-lTxfC4kFF/view?usp=sharing"/>'
 
 '''play transfer to same account command'''
 def voiceTransfer():
     print("playing transfer to same bank message")
+    return '<Say> For transfer, press 2</Say>'
     return '<Play url="https://drive.google.com/file/d/16ZfZXfZmmR9-gcXOezGvqpa9Z3ALR1vW/view?usp=sharing"/>'
     
 
     
 def voiceDeposit():
     print("playing deposit message")
+    return '<Say> For deppist, press 3</Say>'
     return '<Play url="https://drive.google.com/file/d/1-F6aLgRgnytTh3BZbv1dL9NbzZw1sYkD/view?usp=sharing"/>'
+    
     
 def voiceComplain():
     print("playing complaint message")
+    return '<Say> For transfer, press 4</Say>'
     return '<Play url="karaBankiVoice\complain.mp3"/>'
     
 def depositInstruction():
@@ -54,6 +60,7 @@ def depositInstruction():
 
 def exitInstruction():
     print("exiting........")
+    return '<Say> For exit, press 5 or hang up now</Say>'
     return '<Play url="https://drive.google.com/file/d/13UPIVf6IAvQcWCHguE_5PraQsAZUo55c/view?usp=sharing"/>'
 
 '''create an account for a new user'''
@@ -260,45 +267,50 @@ def voice():
    session_id   = request.values.get("sessionId", None)
    isActive  = request.values.get("isActive", None)
    phone_number = ""
+  # <Play url="https://drive.google.com/file/d/1Wnj-ukKcZ7lIiXGzuEDGjI9tshxbnO6D/view?usp=sharing"/>
    response =  '''<Response>
-            <Play url="https://drive.google.com/file/d/1Wnj-ukKcZ7lIiXGzuEDGjI9tshxbnO6D/view?usp=sharing"/>
-            </Response>
+            <Say> Welcome to Karabanki </Say>
+            
             '''
+   print(request.values)
    # if isActive == 1:
    #    phone_number = request.values.get("callerNumber", None)
    
-   # if(phone_number != ""):
-   #    if(isCustomer(phone_number.replace("+234",""))):
-   #       commands = voiceComands()
-   #       for command in commands:
-   #          response += command
-   #       response += "</Response>"
-   #    else:
-   #       commands = voiceOpenAcct()
-   #       response += "</Response>"
+   
+   if(isCustomer(phone_number.replace("+234",""))):
+         commands = voiceComands()
+         for command in commands:
+            response += command
+         response += "</Response>"
+   else:
+         commands = voiceOpenAcct()
+         response += "</Response>"
    # else:
    #    response += "</Response>"
+   print(res)
    return response
+   
    # else:
    #    commands = VoiceAssistant.voiceOpenAcct()
    #    response += ' <GetDigits timeout="30" finishOnKey="#">'
    #    response += '<Say voice="man" playBeep="false">Please enter your account '
    #    response += 'number followed by the hash sign</Say> </GetDigits> </Response>'
 
-   #  dtmfDigits = request.values.get("dtmfDigits", None)
+   # dtmfDigits = request.values.get("dtmfDigits", None)
 
-   #  if dtmfDigits == '1234':
+   # if dtmfDigits == '1234':
    #    response = '<Response> <GetDigits timeout="30" finishOnKey="#">'
    #    response +=' <Say voice="man" playBeep="false"> Press 1 followed by a hash '
    #    response +='sign to get your account balance or 0 followed by a hash sign to'
    #    response += ' quit</Say> </GetDigits></Response>'
 
-   #  elif dtmfDigits == '1':
+   # elif dtmfDigits == '1':
    #    response = '<Response>'
    #    response += '<Say voice="man" playBeep="false" >Your balance is 1234 Shillings</Say>'
    #    response+= '<Reject/> </Response>'
 
-   #  elif dtmfDigits == '0':
+   # elif dtmfDigits == '0':
    #    response = '<Response>'
    #    response += '<Say voice="man" playBeep="false" >Its been a pleasure, good bye </Say>'
    #    response+= '<Reject/> </Response>'
+   # return response
